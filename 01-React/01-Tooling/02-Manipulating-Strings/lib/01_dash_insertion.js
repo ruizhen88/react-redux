8a172a397934028094c14d198c9adc7428a88013
Yejin Jung <yejiin.jung@gmail.com>
2019-03-21 12:08:15 +0800

    challenge#1 finished

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,6 +1,14 @@
 function insertDash(word) {
-  // TODO: implement the method and return word with dashes
-  return null;
+  const wordArr = word.split("");
+  wordArr.forEach((letter, index, array) => {
+    if (array[index + 1] !== undefined) {
+      if ((letter.match(/[aeiou\W]/i) === null) && (array[index + 1].match(/[aeiou\W]/i) === null)) {
+        array.splice(index + 1, 0, "-");
+      }
+    }
+  });
+  return wordArr.join("");
 }
 
+// insertDash("Le Wagon");
 module.exports = insertDash;
